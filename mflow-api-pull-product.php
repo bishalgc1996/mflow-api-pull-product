993c4d0d02cea7add037dedab963eea4936c60b6
bishalgc1996 <bishal.gc.1996@gmail.com>
2023-06-13 12:09:14 +0545

    Final code

--- mflow-api-pull-product.php
+++ mflow-api-pull-product.php
@@ -55,7 +55,7 @@
                     // Check if the product already exists in WooCommerce
                     $existing_product = wc_get_product_id_by_sku($product->sku);
 
-                    if ($count > 22) {
+                    if ($count > 12) {
                         break;
                     }
 
@@ -64,6 +64,8 @@
                     } elseif ($product->type === 'Single') {
                         $this->create_single_product($product);
                     } elseif ($product->type === 'Variable') {
+
+                        
                         $this->create_variable_product($product);
                     }
 
@@ -81,130 +83,82 @@
     }
 
     private function update_product($product_id, $product_data) {
-
-        // get products
         $product_object = wc_get_product($product_id);
-        // set product name
         $product_object->set_name($product_data->name);
         $product_object->set_regular_price($product_data->price);
         $product_object->set_stock_quantity($product_data->stock_quantity);
-        $product_object->set_sku($product_data->sku);
-
-        try {
-            $product_object->save();
-        } catch (WC_Data_Exception $e) {
-            // Handle the exception when saving the product
-            echo 'Error updating product: ' . $e->getMessage();
-        }
+        $product_object->save();
+        update_post_meta($product_id, 'mflow_product_id', $product_data->id);
     }
 
     private function create_single_product($product_data) {
-        // Check if the product already exists in WooCommerce
-        $existing_product = wc_get_product_id_by_sku($product_data->sku);
-
-        if ($existing_product) {
-            // Duplicate SKU found, generate a unique SKU
-            $product_data->sku = $this->generate_unique_sku($product_data->sku);
-        }
-
-        try {
-            $new_product = new WC_Product_Simple();
-            $new_product->set_name($product_data->name);
-            $new_product->set_regular_price($product_data->price);
-            $new_product->set_description($product_data->description);
-            $new_product->set_sku($product_data->sku);
-            $new_product->set_stock_quantity($product_data->stock_quantity);
-            $new_product->save();
-
-            $product_id = $new_product->get_id();
-            update_post_meta($product_id, 'mflow_product_id', $product_data->id);
-            wc_delete_product_transients($product_id);
-        } catch (WC_Data_Exception $e) {
-            // Handle the exception when creating the product
-            echo 'Error creating product: ' . $e->getMessage();
-        }
+        $new_product = new WC_Product_Simple();
+        $new_product->set_name($product_data->name);
+        $new_product->set_regular_price($product_data->price);
+        var_dump($product_data->description) ;
+        $new_product->set_description($product_data->description);
+        $new_product->set_sku($product_data->sku);
+        $new_product->set_stock_quantity($product_data->stock_quantity);
+        $new_product->save();
+        $product_id = $new_product->get_id();
+        update_post_meta($product_id, 'mflow_product_id', $product_data->id);
     }
-
+    
     private function create_variable_product($product_data) {
-        // Check if the product already exists in WooCommerce
-        $existing_product = wc_get_product_id_by_sku($product_data->sku);
-
-        if ($existing_product) {
-            // Duplicate SKU found, generate a unique SKU
-            $product_data->sku = $this->generate_unique_sku($product_data->sku);
-        }
-
-        try {
-            $new_product = new WC_Product_Variable();
-            $new_product->set_name($product_data->name);
-            $new_product->set_description($product_data->description);
-            $new_product->set_sku($product_data->sku);
-
-            // Set attributes for the variable product
-            $attributes = array(
-                'color' => array(
-                    'name'         => 'Color', // Attribute name
-                    'value'        => '', // Attribute initial value (empty)
-                    'is_visible'   => 1, // Show attribute on the product page
-                    'is_variation' => 1, // Use attribute for variations
-                    'is_taxonomy'  => 0 // Not linked to any taxonomy
-                )
-            );
-
-            $new_product->set_attributes($attributes);
-            $new_product->save();
-
-            // Set variations for the variable product
-            $variations = array(
-                array(
-                    'name'           => 'צבע: אפור',
-                    'sku'            => $product_data->sku,
-                    'regular_price'  => '0.00',
-                    'manage_stock'   => 0,
-                    'stock_quantity' => 0,
-                    'image_id'       => 28 // ID of the image for this variation
-                ),
-                array(
-                    'name'           => 'צבע: אדום',
-                    'sku'            => $product_data->sku,
-                    'regular_price'  => '0.00',
-                    'manage_stock'   => 0,
-                    'stock_quantity' => 0,
-                    'image_id'       => 0 // ID of the image for this variation (if available)
-                )
-            );
-
-            foreach ($variations as $variation_data) {
-                $variation = new WC_Product_Variation();
-                $variation->set_parent_id($new_product->get_id());
-                $variation->set_name($variation_data['name']);
-                $variation->set_sku($variation_data['sku']);
-                $variation->set_regular_price($variation_data['regular_price']);
-                $variation->set_manage_stock($variation_data['manage_stock']);
-                $variation->set_stock_quantity($variation_data['stock_quantity']);
-
-                // Set image for the variation if available
-                $image_id = $variation_data['image_id'];
-                if ($image_id) {
-                    $variation->set_image_id($image_id);
-                }
-
-                $variation->save();
+        $new_product = new WC_Product_Variable();
+        $new_product->set_name($product_data->name);
+        $new_product->set_description($product_data->description);
+        $new_product->set_sku($product_data->sku);
+    
+        // Set attributes for the variable product
+        $attributes = array(
+            'color' => array(
+                'name'         => 'Color', // Attribute name
+                'value'        => '', // Attribute initial value (empty)
+                'is_visible'   => 1, // Show attribute on the product page
+                'is_variation' => 1, // Use attribute for variations
+                'is_taxonomy'  => 0 // Not linked to any taxonomy
+            )
+        );
+    
+        $new_product->set_attributes($attributes);
+        $new_product->save();
+    
+        // Get the product ID of the newly created variable product
+        $product_id = $new_product->get_id();
+    
+        // Set variations for the variable product
+        $variations = $product_data->variations;
+    
+        foreach ($variations as $variation_data) {
+            // Check if the variation SKU already exists
+            $existing_variation = wc_get_product_id_by_sku($variation_data->sku);
+    
+            if ($existing_variation) {
+                continue; // Skip creating the variation if the SKU already exists
             }
-
-            $product_id = $new_product->get_id();
-            update_post_meta($product_id, 'mflow_product_id', $product_data->id);
-            wc_delete_product_transients($product_id);
-        } catch (WC_Data_Exception $e) {
-            // Handle the exception when creating the product
-            echo 'Error creating product: ' . $e->getMessage();
+    
+            $variation = new WC_Product_Variation();
+            $variation->set_parent_id($product_id);
+            $variation->set_sku($variation_data->sku);
+            $variation->set_regular_price($variation_data->price);
+            $variation->set_manage_stock($variation_data->manage_stock);
+            $variation->set_stock_quantity($variation_data->stock_quantity);
+    
+            // Set attributes for the variation
+            $variation_attributes = array(
+                'color' => $variation_data->name // Value of the 'color' attribute for this variation
+            );
+    
+            $variation->set_attributes($variation_attributes);
+            $variation->save();
         }
+    
+        // Update the product ID meta for the variable product
+        update_post_meta($product_id, 'mflow_product_id', $product_data->id);
     }
-
-    private function generate_unique_sku($sku) {
-        $suffix = '-' . rand(1000, 9999); // Add a random 4-digit suffix to make the SKU unique
-        return $sku . $suffix;
-    }
+    
+    
 }
 
 // Usage
